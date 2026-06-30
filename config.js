ac9d58b4214c5e4d28ef1bfb5679b05398c13bed
F4ulty12 <149839830+F4ulty12@users.noreply.github.com>
2026-06-30 17:18:25 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const WEBHOOK_URL = "https://discord.com/api/webhooks/1521622347550031873/mUaIPikb-WxXLI1ndvCGRLPZkpgTossN9sdfKqZd7VgPYQo3ZRbu80QsGXse6MeLAMWe";
-
-const WEBHOOK_IS_FORUM_CHANNEL = false;

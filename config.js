996ff0893ae98573fa6b3ce65e390de334fa2e01
F4ulty12 <149839830+F4ulty12@users.noreply.github.com>
2026-06-30 17:04:16 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1521622347550031873/mUaIPikb-WxXLI1ndvCGRLPZkpgTossN9sdfKqZd7VgPYQo3ZRbu80QsGXse6MeLAMWe";
+
+const WEBHOOK_IS_FORUM_CHANNEL = true;
